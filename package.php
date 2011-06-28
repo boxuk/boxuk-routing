59fdc2f2d9578792c5fbf5c9b0fd9896146a205c
rodnaph <rod@pu-gh.com>
2011-06-28 15:04:43 +0100

    Updated PEAR channel

--- package.php
+++ package.php
@@ -15,6 +15,7 @@
 define( 'VERSION', '1.1.0' );
 
 require_once( 'PEAR/PackageFileManager2.php' );
+require_once( 'PEAR/PackageFileManager/File.php' );
 
 $packagexml = new PEAR_PackageFileManager2;
 $packagexml->setOptions(array(
@@ -25,7 +26,7 @@ $packagexml->setOptions(array(
 $packagexml->setPackage( 'routing' );
 $packagexml->setSummary( 'Routing Library' );
 $packagexml->setDescription( '-' );
-$packagexml->setChannel( 'pear.boxuk.com' );
+$packagexml->setChannel( 'pear.boxuk.net' );
 $packagexml->setAPIVersion( VERSION );
 $packagexml->setReleaseVersion( VERSION );
 $packagexml->setReleaseStability( 'stable' );
